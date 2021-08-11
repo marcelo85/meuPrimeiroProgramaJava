628c200407bf5b9438f2d6bc75fa91e62d40d1a0
Marcelo Campos <40444189+marcelo85@users.noreply.github.com>
2021-08-11 18:17:20 -0300

    Update Principal.java

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -28,7 +28,7 @@ public class Principal {
 		// Saldacao especifica para o nome lido.
 		System.out.printf("Ola %s!\n", nome);
 		
-		//Nota Implementar leitura da idade e exibi-la	
+		//Nota Implementar leitura da idade e exibir	
 			
 	}
 
